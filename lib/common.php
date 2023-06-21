885af8a4c90c3dc76ebf4c2455c3a9f31583f0c9
simtaejin <stj53@nate.com>
2023-06-21 20:40:46 +0900

    푸쉬 발송 수정

--- lib/common.php
+++ lib/common.php
@@ -14,12 +14,8 @@ function print_r2($arr) {
 function push_send($push_title, $push_content, $individual = '') {
     global $conn;
 
-    $push_title_arr = array();
-    $push_content_arr = array();
-
-    $app_id = "ad255f06-bf9b-40e7-a20a-69313551d364";
-    $restapi_key = "Y2Y1OGI1YmQtMzM4Yi00MzMwLTgwOTMtOTcyNDQ4NDQ5YjVj";
-
+    $push_url = "http://richpig.wave2.co.kr/AdminLTE/push_history.php";
+    $img_url = "";
     $push_title_arr['en'] = $push_title;
     $push_content_arr['en'] = $push_content;
 
@@ -31,6 +27,29 @@ function push_send($push_title, $push_content, $individual = '') {
         $push_target = [];
         $individual_arr[] = $individual;
     }
+
+    $query = "INSERT INTO `push_send_data` SET
+            push_title='{$push_title}',
+            push_content='{$push_content}',
+            push_url='{$push_url}',
+            img_url='{$img_url}',
+            push_target='{$push_target}',
+            push_id='{$individual}',
+            send_YN='N',
+            create_at=now() ";
+
+    $result = mysqli_query($conn, $query);
+    $last_uid = mysqli_insert_id($conn);
+
+
+
+    $push_title_arr = array();
+    $push_content_arr = array();
+
+    $app_id = "ad255f06-bf9b-40e7-a20a-69313551d364";
+    $restapi_key = "Y2Y1OGI1YmQtMzM4Yi00MzMwLTgwOTMtOTcyNDQ4NDQ5YjVj";
+
+
 
     $data['']= '';
 
@@ -51,24 +70,25 @@ function push_send($push_title, $push_content, $individual = '') {
     //print_r($body);exit;
     $body = json_encode($body);
 
+    if ($last_uid) {
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL			    => $url, //URL 지정하기
+            CURLOPT_POST			=> true, //true시 post 전송
+            CURLOPT_RETURNTRANSFER	=> true, //요청 결과를 문자열로 반환
+            CURLOPT_HTTPHEADER		=> array(// header data
+                "Content-Type:application/json",
+                "Authorization: Basic ".$restapi_key
+            ),
+            CURLOPT_SSL_VERIFYPEER	=> 0,    //원격 서버의 인증서가 유효한지 검사 안함
+            CURLOPT_POSTFIELDS		=> $body //POST data
+        ));
 
-    $ch = curl_init();
-    curl_setopt_array($ch, array(
-        CURLOPT_URL			    => $url, //URL 지정하기
-        CURLOPT_POST			=> true, //true시 post 전송
-        CURLOPT_RETURNTRANSFER	=> true, //요청 결과를 문자열로 반환
-        CURLOPT_HTTPHEADER		=> array(// header data
-            "Content-Type:application/json",
-            "Authorization: Basic ".$restapi_key
-        ),
-        CURLOPT_SSL_VERIFYPEER	=> 0,    //원격 서버의 인증서가 유효한지 검사 안함
-        CURLOPT_POSTFIELDS		=> $body //POST data
-    ));
+        $response = curl_exec($ch);
+        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-    $response = curl_exec($ch);
-    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-    $response= json_decode($response,true);
+        $response= json_decode($response,true);
+    }
 
     if ($response['id'] && $status_code == 200 ) {
         $query = "UPDATE `push_send_data` SET send_YN='Y', update_at=now() where idx = {$last_uid} ";
