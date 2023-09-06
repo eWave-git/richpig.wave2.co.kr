b845642f6265197f8c15f9d4ddc8a1b054e6525c
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 <stj53@nate.com>
2023-09-06 14:54:42 +0900

    회원별 시스템 설정 수정

--- AdminLTE/data/data_system.php
+++ AdminLTE/data/data_system.php
@@ -4,23 +4,24 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
+                <?php
+                $query = "select * from `system_data` where member_id = '{$_SESSION['user_id']}'";
+                $result = mysqli_query($conn, $query);
+                $row = mysqli_fetch_array($result);
+                $row['push_use_YN'] = $row['push_use_YN'] ?? 'N';
+                $mode = $row['member_id'] ? 'update':'create';
+                ?>
                 <form action="../conf/systemAction.php" method="post">
-                    <input type="hidden" name="mode" value="create">
+                    <input type="hidden" name="mode" value="<?=$mode?>">
+                    <input type="hidden" name="member_id" value="<?=$_SESSION['user_id']?>">
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title">시스템 설정</h3>
                         </div>
-                        <?php
-                        $query = "select * from `system_data`";
-                        $result = mysqli_query($conn, $query);
-                        $row = mysqli_fetch_array($result);
 
-                        ?>
                         <div class="card-body table-responsive">
                             <div class="card-body row">
                                 <div class="col-12">
-
-
                                     <div class="form-group">
                                         <label for="inputSubject">push 사용유무</label>
                                         <select class="form-control float-right" name="push_use_YN">
--- conf/systemAction.php
+++ conf/systemAction.php
@@ -7,11 +7,19 @@ foreach ($_REQUEST as $k => $v) {
     $$k = $v;
 }
 
-
-$query = "UPDATE `system_data` SET 
+if ($mode == "create") {
+    $query = "INSERT `system_data` SET
+             member_id='{$member_id}',
              push_use_YN='{$push_use_YN}',
             update_at=now() ";
+} else if ($mode == "update") {
+    $query = "UPDATE `system_data` SET
+                 push_use_YN='{$push_use_YN}',
+                update_at=now() where member_id='{$member_id}'";
+}
+
 $result = mysqli_query($conn, $query);
+
 
 
 header("Location:../AdminLTE/system.php");
